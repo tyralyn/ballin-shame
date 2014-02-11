193368cea2627c61a37b59ae9cfee1b44a94ab9a
tyralyn <tyralyntran@gmail.com>
2014-02-10 23:19:03 -0800

    writing and reading functional

--- audiosym.py
+++ audiosym.py
@@ -1,10 +1,48 @@
 import sunau
+import math
+import audioop
 
-w = sunau.open("soundfiles/syringe.au", "r")
+def muOperation (x):
+    if x<0:
+        sign = -1
+    else:
+        sign = 1
+    muValue = sign * math.log(1+255*abs(x)) / math.log(1+255)
+    return muValue
 
-if w.getnchannels() == 1: print ("mono," , w.getnchannels())
-else:
-    print ("stereo," , w.getnchannels())
+
+w = sunau.open("soundfiles/whatYouFeel.au", "r")
 
 print (w.getsampwidth()*8 , "bits,")
+print (w.getnframes(), "frames,")
 print (w.getframerate() , "Hz sampling rate")
+print (w.getcompname(), "compression")
+nframes = math.floor(w.getnframes())
+
+k = sunau.open("soundfiles/whatYouFeel2.au", "w")
+k.setnchannels(w.getnchannels())
+k.setframerate(w.getframerate())
+k.setsampwidth(w.getsampwidth())
+k.setnframes(w.getnframes())
+k.setcomptype(w.getcomptype(),w.getcompname())
+
+audiostring = w.readframes(nframes)
+k.writeframes(audiostring)
+
+list = []
+for l in audiostring:
+    list.append(l)
+print ("list length:", len(list))
+threshold = 2**16-1
+list2 = []
+for i in range (80000, 80050, 2):
+    normalizedValue=(((audiostring[i]*256+audiostring[i+1])-(threshold/2))/(threshold/2))
+    muValue = muOperation(normalizedValue)
+    list2.append(muValue)
+for item in list2:
+    print(str(item).encode())
+    
+
+
+
+
