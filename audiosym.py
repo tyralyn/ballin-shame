d530954831c09dcf7b6c122c6d9c95fa96759d2d
tyralyn <tyralyntran@gmail.com>
2014-02-11 16:53:28 -0800

    testing out audioop methods, don't work yet

--- audiosym.py
+++ audiosym.py
@@ -10,13 +10,19 @@ def muOperation (x):
     muValue = sign * math.log(1+255*abs(x)) / math.log(1+255)
     return muValue
 
+def muOperation2 (x):
+    return audioop.lin2ulaw(x, 1)
+
+
 
 w = sunau.open("soundfiles/whatYouFeel.au", "r")
-
 print (w.getsampwidth()*8 , "bits,")
 print (w.getnframes(), "frames,")
 print (w.getframerate() , "Hz sampling rate")
 print (w.getcompname(), "compression")
+print (w.getnchannels(), "channels")
+print (w.getcomptype(),w.getcompname())
+
 nframes = math.floor(w.getnframes())
 
 k = sunau.open("soundfiles/whatYouFeel2.au", "w")
@@ -27,21 +33,20 @@ k.setnframes(w.getnframes())
 k.setcomptype(w.getcomptype(),w.getcompname())
 
 audiostring = w.readframes(nframes)
-k.writeframes(audiostring)
+#k.writeframes(audiostring)
 
-list = []
-for l in audiostring:
-    list.append(l)
-print ("list length:", len(list))
-threshold = 2**16-1
-list2 = []
-for i in range (80000, 80050, 2):
-    normalizedValue=(((audiostring[i]*256+audiostring[i+1])-(threshold/2))/(threshold/2))
-    muValue = muOperation(normalizedValue)
-    list2.append(muValue)
-for item in list2:
-    print(str(item).encode())
-    
+list3 = audioop.lin2lin(audiostring, 2, 1)
+
+m = sunau.open("soundfiles/whatYouFeel3.au", "w")
+m.setnchannels(w.getnchannels())
+m.setframerate(w.getframerate())
+m.setsampwidth(1)
+m.setcomptype('ULAW', w.getcompname())
+m.writeframes(list3)
+
+list4 = audioop.lin2lin(list3, 1, 2)
+k.writeframes(list4)
+
 
 
 
