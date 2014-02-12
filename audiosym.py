6b27c56088f8875814ac55678aaabcad08c1a129
tyralyn <tyralyntran@gmail.com>
2014-02-11 21:28:27 -0800

    writes to two separate files, one for each replacement method

--- audiosym.py
+++ audiosym.py
@@ -4,43 +4,62 @@ import audioop
 import time
 import random
 
-def makePacket(prev):
-    randnum = random.random()
-    if (randnum<lossratio):
-        #print("<")
-        j.writeframes(replacement)
-    else:
-        newEnd = prev+packetsize
-        j.writeframes(recodedaudio[prev:newEnd])
-        #print(">")
-    prev = prev+packetsize
-
 #initializing global variables
 nframes=0;
 w = sunau.open("soundfiles/whatYouFeel.au", "r")
-j= sunau.open("soundfiles/whatYouFeel3.au","w")
+j= sunau.open("soundfiles/whatYouFeel0.au","w")
+k= sunau.open("soundfiles/whatYouFeel1.au","w")
 currentIndex = 0;
 replacement = b'\x00'
-packetsize = 200
+packetsize = 100
 prev=0
-lossratio = 0.0
+lossratio = 0.50
+zeroes = False;
 
 nframes = math.floor(w.getnframes())
 audiostring = w.readframes(nframes)
-uLawAudio = audioop.lin2ulaw(audiostring, 1)
-recodedaudio = audioop.ulaw2lin(uLawAudio, 1)
+encodedaudio = audioop.lin2ulaw(audiostring, 1)
+
+recodedaudio = audioop.ulaw2lin(encodedaudio, 1)
 
 j.setsampwidth(w.getsampwidth())
 j.setframerate(w.getframerate())
 j.setcomptype(w.getcomptype(),w.getcompname())
 j.setnchannels(w.getnchannels())
 
-for i in range (0, packetsize):
+k.setsampwidth(w.getsampwidth())
+k.setframerate(w.getframerate())
+k.setcomptype(w.getcomptype(),w.getcompname())
+k.setnchannels(w.getnchannels())
+
+'''for i in range (0, packetsize):
     replacement+=b'\x00'
 
 
 for i in range (0, nframes, packetsize):
-    makePacket(prev)
+    makePacket(prev)'''
 
+def sendPacket(packetsize, lossratio):
+    previousend = 0;
+    pos = j.tell()
+    t0 = time.time()
+    singleByte = b'\x00'
+    replacement = singleByte
+    newEnd = 0
+    for i in range (1, packetsize):
+        replacement+=singleByte
+    for i in range (0, 2*nframes, packetsize):
+        randnum = random.random()
+        if (randnum<lossratio):
+            j.writeframes(recodedaudio[previousend-packetsize:newEnd])
+            k.writeframes(replacement)
+            previousend = previousend+packetsize
+        else:
+            newEnd = previousend+packetsize
+            j.writeframes(recodedaudio[previousend:newEnd])
+            k.writeframes(recodedaudio[previousend:newEnd])
+            previousend = previousend+packetsize
+    print("time:",t0, time.time())
+    
 
-print("finished!")
+sendPacket(packetsize, lossratio)
