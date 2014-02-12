dca68c72249417b93a63b945cc009ee3ae64f94c
tyralyn <tyralyntran@gmail.com>
2014-02-11 20:23:32 -0800

    basic simulation scheme completed

--- audiosym.py
+++ audiosym.py
@@ -1,35 +1,15 @@
 import sunau
 import math
 import audioop
+import time
+import random
 
-
-muValueList =[0,0,1,1,2,2,2,2,3,3,3,3,3,3,3,3, 
-     4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-     5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
-     5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
-     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7]
-
-
-def normalize(x):
-    threshold = 2*16-1
-    normalizedValue = (x-(threshold/2))/(threshold/2)
-    return normalizedValue
-    
-
-
-
+nframes=0;
 w = sunau.open("soundfiles/whatYouFeel.au", "r")
+k = sunau.open("soundfiles/whatYouFeel2.au","w")
+j= sunau.open("soundfiles/whatYouFeel3.au","w")
+currentIndex = 0;
+
 print (w.getsampwidth()*8 , "bits,")
 print (w.getnframes(), "frames,")
 print (w.getframerate() , "Hz sampling rate")
@@ -40,32 +20,44 @@ print (w.getcomptype(),w.getcompname())
 nframes = math.floor(w.getnframes())
 audiostring = w.readframes(nframes)
 
-audiolist = []
-for l in audiostring:
-    audiolist.append(l)
-
 uLawAudio = audioop.lin2ulaw(audiostring, 1)
 
-ulist = []
-for i in uLawAudio:
-    ulist.append(i)
-
-for i in range(60060, 60100):
-    print(ulist[i], audiolist[i])
-
-k = sunau.open("soundfiles/whatYouFeel2.au","w")
-
-k.setsampwidth(w.getsampwidth())
+'''k.setsampwidth(w.getsampwidth())
 k.setframerate(w.getframerate())
 k.setcomptype(w.getcomptype(),w.getcompname())
 k.setnchannels(w.getnchannels())
 
+
+
+k.writeframes(recodedaudio)'''
+
 recodedaudio = audioop.ulaw2lin(uLawAudio, 1)
 
-k.writeframes(recodedaudio)
+j.setsampwidth(w.getsampwidth())
+j.setframerate(w.getframerate())
+j.setcomptype(w.getcomptype(),w.getcompname())
+j.setnchannels(w.getnchannels())
 
-j= sunau.open("soundfiles/whatYouFeel3.au","w")
+def sendPacket(packetsize, lossratio):
+    previousend = 0;
+    pos = j.tell()
+    t0 = time.time()
+    singleByte = b'\x00'
+    replacement = singleByte
+    for i in range (0, packetsize):
+        replacement+=singleByte
+        j.writeframes(replacement)
+    for i in range (0, nframes, packetsize):
+        randnum = random.random()
+        if (randnum>lossratio):
+            previousend = previousend+packetsize
+            print("cont")
+            continue
+        else:
+            newEnd = previousend+packetsize
+            j.writeframes(recodedaudio[previousend:newEnd])
+            previousend = previousend+packetsize
+    print("time:",t0, time.time())
+    
 
-
-
-
+sendPacket(200, .75)
